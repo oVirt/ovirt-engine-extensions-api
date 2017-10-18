4b3f8b7dda96b7e1a06b3b3f87647a810094b988
Martin Perina <mperina@redhat.com>
2017-10-18 22:46:45 +0200

    Add oVirt Engine Extensions API classes
    1. Import classes from ovirt-engine project
    2. Fix copyright headers
    3. Bump requirements to OpenJDK 11
    
    Change-Id: I78938f1f264f7097a2db7f31445617d37296fd30
    Signed-off-by: Martin Perina <mperina@redhat.com>

---
+++ src/main/java/org/ovirt/engine/api/extensions/aaa/package-info.java
@@ -0,0 +1,8 @@
+/*
+ * Copyright oVirt Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+/**
+ * Authentication, Authorization and Accounting interfaces.
+ */
+package org.ovirt.engine.api.extensions.aaa;
---
+++ src/main/java/org/ovirt/engine/api/extensions/logger/package-info.java
@@ -0,0 +1,8 @@
+/*
+ * Copyright oVirt Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+/**
+ * Logger interfaces.
+ */
+package org.ovirt.engine.api.extensions.logger;
