b2d758dd7f5d52de08a21a46754a0476af42a849
Martin Perina <mperina@redhat.com>
2021-12-10 23:20:33 +0100

    Move GIT_HASH computation into build-artifacts.sh
    Signed-off-by: Martin Perina <mperina@redhat.com>

--- .automation/build-artifacts.sh
+++ .automation/build-artifacts.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -xe
 
 # git hash of current commit should be passed as the 1st paraameter
-GIT_HASH=$1
+if [ "${GITHUB_SHA}" == "" ]; then
+  GIT_HASH=$(git rev-list HEAD | wc -l)
+else
+  GIT_HASH=$(git rev-parse --short $GITHUB_SHA)
+fi
 
-# Directory, where build artifacts will be stored, should be passed as the 2nd parameter
-ARTIFACTS_DIR=${2:-exported-artifacts}
+# Directory, where build artifacts will be stored, should be passed as the 1st parameter
+ARTIFACTS_DIR=${1:-exported-artifacts}
 
 # Prepare the version string (with support for SNAPSHOT versioning)
 VERSION=$(mvn help:evaluate  -q -DforceStdout -Dexpression=project.version)
